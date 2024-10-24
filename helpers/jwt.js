13588448bbbbd3e230548997d95f22266333df14
Henry Cañizares <henrycaba471@gmail.com>
2024-10-24 07:04:25 -0500

    Se actualizo tikets

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,8 +2,13 @@ const jwt = require('jwt-simple');
 const moment = require('moment');
 
 const secretKey = process.env.SECRET_KEY;
+console.log('SECRET_KEY:', secretKey); // Agregar este log para depurar
 
 const createToken = (user) => {
+    if (!secretKey) {
+        throw new Error("SECRET_KEY is not defined"); // Lanzar un error si la clave no está definida
+    }
+
     const payload = {
         id: user._id,
         name: user.name,
@@ -13,7 +18,8 @@ const createToken = (user) => {
         profile: user.image,
         iat: moment().unix(),
         exp: moment().add(30, "minutes").unix()
-    }
+    };
+
     return jwt.encode(payload, secretKey);
 }
 
